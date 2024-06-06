0c88d74fe40276e6dd7acac6bb8257b92575614c
Sneha Sonkar <sonkarsneha24@gmail.com>
2024-06-06 12:20:56 +0530

    search bar1

--- lib/searchbar.dart
+++ lib/searchbar.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/main.dart';
 
-class _MyAppState extends State<MyApp> {
+class Searchbar extends StatefulWidget {
+  const Searchbar({super.key});
+
+  @override
+  State<Searchbar> createState() => _MyAppState();
+
+
+class _MyAppState extends State<Searchbar> {
   bool isDark = false;
 
   @override
@@ -67,132 +74,132 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class NavigationBarApp extends StatelessWidget {
-  const NavigationBarApp({super.key});
+// class NavigationBarApp extends StatelessWidget {
+//   const NavigationBarApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const NavigationExample(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(useMaterial3: true),
+//       home: const NavigationExample(),
+//     );
+//   }
+// }
 
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
+// class NavigationExample extends StatefulWidget {
+//   const NavigationExample({super.key});
 
-  @override
-  State<NavigationExample> createState() => _NavigationExampleState();
-}
+//   @override
+//   State<NavigationExample> createState() => _NavigationExampleState();
+// }
 
-class _NavigationExampleState extends State<NavigationExample> {
-  int currentPageIndex = 0;
+// class _NavigationExampleState extends State<NavigationExample> {
+//   int currentPageIndex = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.amber,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Badge(child: Icon(Icons.notifications_sharp)),
-            label: 'Notifications',
-          ),
-                  ],
-      ),
-      body: <Widget>[
-        /// Home page
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-                style: theme.textTheme.titleLarge,
-              ),
-            ),
-          ),
-        ),
+//   @override
+//   Widget build(BuildContext context) {
+//     final ThemeData theme = Theme.of(context);
+//     return Scaffold(
+//       bottomNavigationBar: NavigationBar(
+//         onDestinationSelected: (int index) {
+//           setState(() {
+//             currentPageIndex = index;
+//           });
+//         },
+//         indicatorColor: Colors.amber,
+//         selectedIndex: currentPageIndex,
+//         destinations: const <Widget>[
+//           NavigationDestination(
+//             selectedIcon: Icon(Icons.home),
+//             icon: Icon(Icons.home_outlined),
+//             label: 'Home',
+//           ),
+//           NavigationDestination(
+//             icon: Badge(child: Icon(Icons.notifications_sharp)),
+//             label: 'Notifications',
+//           ),
+//                   ],
+//       ),
+//       body: <Widget>[
+//         /// Home page
+//         Card(
+//           shadowColor: Colors.transparent,
+//           margin: const EdgeInsets.all(8.0),
+//           child: SizedBox.expand(
+//             child: Center(
+//               child: Text(
+//                 'Home page',
+//                 style: theme.textTheme.titleLarge,
+//               ),
+//             ),
+//           ),
+//         ),
 
-        /// Notifications page
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 1'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 2'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-            ],
-          ),
-        ),
+//         /// Notifications page
+//         const Padding(
+//           padding: EdgeInsets.all(8.0),
+//           child: Column(
+//             children: <Widget>[
+//               Card(
+//                 child: ListTile(
+//                   leading: Icon(Icons.notifications_sharp),
+//                   title: Text('Notification 1'),
+//                   subtitle: Text('This is a notification'),
+//                 ),
+//               ),
+//               Card(
+//                 child: ListTile(
+//                   leading: Icon(Icons.notifications_sharp),
+//                   title: Text('Notification 2'),
+//                   subtitle: Text('This is a notification'),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
 
-        /// Messages page
-        ListView.builder(
-          reverse: true,
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  margin: const EdgeInsets.all(8.0),
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    'Hello',
-                    style: theme.textTheme.bodyLarge!
-                        .copyWith(color: theme.colorScheme.onPrimary),
-                  ),
-                ),
-              );
-            }
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Hi!',
-                  style: theme.textTheme.bodyLarge!
-                      .copyWith(color: theme.colorScheme.onPrimary),
-                ),
-              ),
-            );
-          },
-        ),
-      ][currentPageIndex],
-    );
-  }
-}
+//         /// Messages page
+//         ListView.builder(
+//           reverse: true,
+//           itemCount: 2,
+//           itemBuilder: (BuildContext context, int index) {
+//             if (index == 0) {
+//               return Align(
+//                 alignment: Alignment.centerRight,
+//                 child: Container(
+//                   margin: const EdgeInsets.all(8.0),
+//                   padding: const EdgeInsets.all(8.0),
+//                   decoration: BoxDecoration(
+//                     color: theme.colorScheme.primary,
+//                     borderRadius: BorderRadius.circular(8.0),
+//                   ),
+//                   child: Text(
+//                     'Hello',
+//                     style: theme.textTheme.bodyLarge!
+//                         .copyWith(color: theme.colorScheme.onPrimary),
+//                   ),
+//                 ),
+//               );
+//             }
+//             return Align(
+//               alignment: Alignment.centerLeft,
+//               child: Container(
+//                 margin: const EdgeInsets.all(8.0),
+//                 padding: const EdgeInsets.all(8.0),
+//                 decoration: BoxDecoration(
+//                   color: theme.colorScheme.primary,
+//                   borderRadius: BorderRadius.circular(8.0),
+//                 ),
+//                 child: Text(
+//                   'Hi!',
+//                   style: theme.textTheme.bodyLarge!
+//                       .copyWith(color: theme.colorScheme.onPrimary),
+//                 ),
+//               ),
+//             );
+//           },
+//         ),
+//       ][currentPageIndex],
+//     );
+//   }
+// }
