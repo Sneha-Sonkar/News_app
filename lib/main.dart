1c17af7e07c51e560d956b7673a7f0a7cfba7022
Suyash <beingsuyashar@gmail.com>
2024-06-06 12:42:19 +0530

    fixed errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/bottomTabNavigator.dart';
+import 'package:news_app/searchbar.dart';
+
 /// Flutter code sample for [SearchBar].
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // @override
-  // State<MyApp> createState() => _MyAppState();
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       title: 'News App',
       debugShowCheckedModeBanner: false,
-      home: const Searchbar(title: 'News App'),
-      );
+      home: BottomNavigationBarExampleApp(),
+    );
   }
-
 }
--- lib/searchbar.dart
+++ lib/searchbar.dart
@@ -6,7 +6,7 @@ class Searchbar extends StatefulWidget {
 
   @override
   State<Searchbar> createState() => _MyAppState();
-
+}
 
 class _MyAppState extends State<Searchbar> {
   bool isDark = false;
@@ -14,7 +14,6 @@ class _MyAppState extends State<Searchbar> {
   @override
   Widget build(BuildContext context) {
     final ThemeData themeData = ThemeData(
-           
         useMaterial3: true,
         brightness: isDark ? Brightness.dark : Brightness.light);
 
@@ -73,133 +72,3 @@ class _MyAppState extends State<Searchbar> {
     );
   }
 }
-
-// class NavigationBarApp extends StatelessWidget {
-//   const NavigationBarApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(useMaterial3: true),
-//       home: const NavigationExample(),
-//     );
-//   }
-// }
-
-// class NavigationExample extends StatefulWidget {
-//   const NavigationExample({super.key});
-
-//   @override
-//   State<NavigationExample> createState() => _NavigationExampleState();
-// }
-
-// class _NavigationExampleState extends State<NavigationExample> {
-//   int currentPageIndex = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final ThemeData theme = Theme.of(context);
-//     return Scaffold(
-//       bottomNavigationBar: NavigationBar(
-//         onDestinationSelected: (int index) {
-//           setState(() {
-//             currentPageIndex = index;
-//           });
-//         },
-//         indicatorColor: Colors.amber,
-//         selectedIndex: currentPageIndex,
-//         destinations: const <Widget>[
-//           NavigationDestination(
-//             selectedIcon: Icon(Icons.home),
-//             icon: Icon(Icons.home_outlined),
-//             label: 'Home',
-//           ),
-//           NavigationDestination(
-//             icon: Badge(child: Icon(Icons.notifications_sharp)),
-//             label: 'Notifications',
-//           ),
-//                   ],
-//       ),
-//       body: <Widget>[
-//         /// Home page
-//         Card(
-//           shadowColor: Colors.transparent,
-//           margin: const EdgeInsets.all(8.0),
-//           child: SizedBox.expand(
-//             child: Center(
-//               child: Text(
-//                 'Home page',
-//                 style: theme.textTheme.titleLarge,
-//               ),
-//             ),
-//           ),
-//         ),
-
-//         /// Notifications page
-//         const Padding(
-//           padding: EdgeInsets.all(8.0),
-//           child: Column(
-//             children: <Widget>[
-//               Card(
-//                 child: ListTile(
-//                   leading: Icon(Icons.notifications_sharp),
-//                   title: Text('Notification 1'),
-//                   subtitle: Text('This is a notification'),
-//                 ),
-//               ),
-//               Card(
-//                 child: ListTile(
-//                   leading: Icon(Icons.notifications_sharp),
-//                   title: Text('Notification 2'),
-//                   subtitle: Text('This is a notification'),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-
-//         /// Messages page
-//         ListView.builder(
-//           reverse: true,
-//           itemCount: 2,
-//           itemBuilder: (BuildContext context, int index) {
-//             if (index == 0) {
-//               return Align(
-//                 alignment: Alignment.centerRight,
-//                 child: Container(
-//                   margin: const EdgeInsets.all(8.0),
-//                   padding: const EdgeInsets.all(8.0),
-//                   decoration: BoxDecoration(
-//                     color: theme.colorScheme.primary,
-//                     borderRadius: BorderRadius.circular(8.0),
-//                   ),
-//                   child: Text(
-//                     'Hello',
-//                     style: theme.textTheme.bodyLarge!
-//                         .copyWith(color: theme.colorScheme.onPrimary),
-//                   ),
-//                 ),
-//               );
-//             }
-//             return Align(
-//               alignment: Alignment.centerLeft,
-//               child: Container(
-//                 margin: const EdgeInsets.all(8.0),
-//                 padding: const EdgeInsets.all(8.0),
-//                 decoration: BoxDecoration(
-//                   color: theme.colorScheme.primary,
-//                   borderRadius: BorderRadius.circular(8.0),
-//                 ),
-//                 child: Text(
-//                   'Hi!',
-//                   style: theme.textTheme.bodyLarge!
-//                       .copyWith(color: theme.colorScheme.onPrimary),
-//                 ),
-//               ),
-//             );
-//           },
-//         ),
-//       ][currentPageIndex],
-//     );
-//   }
-// }
